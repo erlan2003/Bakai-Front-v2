e05927f91263c666dd4bc2375969677488fe2333
Kamiev <kamiev29@gmail.com>
2024-08-19 14:22:58 +0600

    добавил первичную верстку для компонентов отчеты и сотрудники, изменил формат кнопок на svg

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-reports',
+  selector: 'app-reports',  // измените на 'app-reports'
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss'],
+  styleUrls: ['./reports.component.scss']
 })
-export class ReportsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ReportsComponent {  // измените на ReportsComponent
+  // Добавляем массив employees с примерными данными
+  employees = [
+    { lastName: 'Иванов', firstName: 'Иван', middleName: 'Иванович', team: 'Команда 1', position: 'Должность 1', location: 'Место 1', date: '01.08.2024' },
+    { lastName: 'Петров', firstName: 'Петр', middleName: 'Петрович', team: 'Команда 2', position: 'Должность 2', location: 'Место 2', date: '02.08.2024' },
+    // Добавьте другие данные по необходимости
+  ];
 }
